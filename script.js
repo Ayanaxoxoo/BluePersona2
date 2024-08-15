bc05652652aa1522bca09c89a83d3d210651d10d
Ayanaxoxoo <165037059+Ayanaxoxoo@users.noreply.github.com>
2024-08-15 21:25:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 var agree_number = 0
+var disagree_number = 0
 
-function agreecounter(){
-var selected_buttons = document.querySelectorAll('input[type="radio"]:checked[value="agree"]');
-agree_number= selected_buttons.length 
-document.getElementById("agreecount").innerHTML = agree_number 
+function eicounter(){
+var selected_buttons_a = document.querySelectorAll('input[type="radio"]:checked[value="agree"]');
+agree_number= selected_buttons_a.length 
+
+
+var selected_buttons_d =document.querySelectorAll('input[type="radio"]:checked[value="disagree"]');
+disagree_number= selected_buttons_d.length 
+
+return(disagree_number)
+}
+
+
+
+var agreeSN_number = 0
+function agreeSNcount(){
+var selected_buttonsSN = document.querySelectorAll('input[type="radio"]:checked[value="agreeSN"]');
+agreeSN_number = selected_buttonsSN.length 
+document.getElementById("agreecountSN").innerHTML = disagree_number
+
+console.log(disagree_number)
 }
